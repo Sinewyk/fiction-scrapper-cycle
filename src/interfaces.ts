d994cc1c7acf9644ce9e54570d96912409a1ed63
sinewyk <sinewyk@gmail.com>
2017-11-16 22:14:00 +0100

    edit interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,7 +1,8 @@
 import { Stream } from 'xstream'
 import { HTTPSource, RequestOptions } from '@cycle/http'
 
+export type HTTPSinkEvent = string | RequestOptions
 export type ConsoleSourceOrSink = Stream<string>
-export type HTTPSink = Stream<string | RequestOptions>
+export type HTTPSink = Stream<HTTPSinkEvent>
 export type InitialDataStream = Stream<string>
 export type HTTPSource = HTTPSource
