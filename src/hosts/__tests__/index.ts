06472e597f10908d9eda48709201a59d76bfd98e
sinewyk <sinewyk@gmail.com>
2017-11-19 14:42:31 +0100

    more testing

--- src/hosts/__tests__/index.ts
+++ src/hosts/__tests__/index.ts
@@ -1,23 +1,51 @@
 import * as wuxia from '../www.wuxiaworld.com'
 
-const harness = [
-  {
-    host: wuxia,
-    name: 'getChapterUrl',
-    initialUrl: 'https://www.wuxiaworld.com/foo/foo-chapter-1',
-    expectedForChapter2: 'https://www.wuxiaworld.com/foo/foo-chapter-2',
-  },
-  {
-    host: wuxia,
-    name: 'getChapterUrl force https',
-    initialUrl: 'http://www.wuxiaworld.com/foo/foo-chapter-1',
-    expectedForChapter2: 'https://www.wuxiaworld.com/foo/foo-chapter-2',
-  },
-]
+import { getBookConf } from '../'
 
-harness.forEach(testConf => {
-  it(`${testConf.host.hostname} ${testConf.name}`, () => {
-    const bookConf = wuxia.createBookConf(testConf.initialUrl)
-    expect(bookConf.getChapterUrl(2)).toEqual(testConf.expectedForChapter2)
+describe('hosts', () => {
+  const harness = [
+    {
+      hostname: 'www.wuxiaworld.com',
+      factory: wuxia,
+      tests: [
+        {
+          name: 'getChapterUrl',
+          initialUrl: 'https://www.wuxiaworld.com/foo/foo-chapter-1',
+          expectedForChapter2: 'https://www.wuxiaworld.com/foo/foo-chapter-2',
+        },
+        {
+          name: 'getChapterUrl force https',
+          initialUrl: 'http://www.wuxiaworld.com/foo/foo-chapter-1',
+          expectedForChapter2: 'https://www.wuxiaworld.com/foo/foo-chapter-2',
+        },
+      ],
+    },
+  ]
+
+  harness.forEach(testConf => {
+    describe(`${testConf.hostname}`, () => {
+      testConf.tests.forEach(test => {
+        it(`${test.name}`, () => {
+          const bookConf = wuxia.createBookConf(test.initialUrl)
+          expect(bookConf.getChapterUrl(2)).toEqual(test.expectedForChapter2)
+        })
+      })
+    })
+  })
+})
+
+describe('#getBookConf', () => {
+  it('can find a book conf', () => {
+    expect(
+      getBookConf('http://www.wuxiaworld.com/foo/foo-chapter-1'),
+    ).toBeTruthy()
+  })
+
+  it('throws on unrecognized hostname', () => {
+    expect(() => getBookConf('http://foo.com')).toThrow()
+  })
+
+  it('throws on unrecognized string', () => {
+    expect(() => getBookConf('foo')).toThrow()
   })
 })
